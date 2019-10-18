b204866f7616d6d56c5c676c2966109b1b3e1df7
=c03er <=n.matheuscarvalho@gmail.com>
2019-10-17 23:33:36 -0300

    mirrow row functin

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,17 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> building_grid
+  end
+
+  def building_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+  end
+
+  def mirrow_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
   end
 
   def pick_color(%Identicon.Image{hex: [r,g,b | _tail]} = image) do
@@ -12,7 +23,6 @@ defmodule Identicon do
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
-    
     %Identicon.Image{hex: hex}
   end
 end
